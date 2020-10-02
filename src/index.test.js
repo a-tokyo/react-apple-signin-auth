a6cbb5b6eccad65d09bee4791c1d547dc75bb31d
Tokyo <ahmed.tokyo1@gmail.com>
2020-10-02 04:41:51 +0200

    more tests

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+import AppleSigninButton, { appleAuthHelpers } from './index';
+
+describe('index', () => {
+  it('exports all modules', () => {
+    expect(AppleSigninButton).toEqual(expect.any(Function));
+    expect(appleAuthHelpers).toEqual(expect.any(Object));
+  });
+});
