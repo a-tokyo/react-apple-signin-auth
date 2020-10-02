1702787535e385b8580a8e58011cfcfdad1b514d
Tokyo <ahmed.tokyo1@gmail.com>
2020-10-02 05:11:17 +0200

    enhance

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,34 @@
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-    template: path.join(__dirname, "demo/src/index.html"),
-    filename: "./index.html"
+  template: path.join(__dirname, 'demo/src/index.html'),
+  filename: './index.html',
 });
+
 module.exports = {
-    entry: path.join(__dirname, "demo/src/index.js"),
-    output: {
-        path: path.join(__dirname, "demo/dist"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: "babel-loader",
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    },
-    plugins: [htmlWebpackPlugin],
-    resolve: {
-        extensions: [".js", ".jsx"]
-    },
-    devServer: {
-        port: 3001
-    }
+  entry: path.join(__dirname, 'demo/src/index.js'),
+  output: {
+    path: path.join(__dirname, 'demo/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    port: 3001,
+  },
 };
