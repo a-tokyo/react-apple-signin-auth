364f6549779865aa1106c550a4c8164a83322007
Ahmed Tarek <ahmed.tokyo1@gmail.com>
2022-03-28 15:49:49 +0200

    🐛 fix logic post contibs + add more tests (#74)
    * add logs
    
    * logs gh
    
    * :bug: fix logic post contribs

--- src/appleAuthHelpers/index.js
+++ src/appleAuthHelpers/index.js
@@ -28,24 +28,26 @@ const signIn = ({
       /** Init apple auth */
       window.AppleID.auth.init(authOptions);
       /** Signin to appleID */
-      window.AppleID.auth.signIn().then((response) => {
-        /** This is only called in case usePopup is true */
-        if (onSuccess) {
-          onSuccess(response);
-        }
-        /** resolve with the reponse */
-        return response;
-      }).catch((err) => {
-        if (onError) {
-          /** Call onError catching the error */
-          onError(err);
-        } else {
-          /** Log the error to help debug */
-          console.error(err);
-        }
-  
-        return null;
-      });
+      return window.AppleID.auth
+        .signIn()
+        .then((response) => {
+          /** This is only called in case usePopup is true */
+          if (onSuccess) {
+            onSuccess(response);
+          }
+          /** resolve with the reponse */
+          return response;
+        })
+        .catch((err) => {
+          if (onError) {
+            /** Call onError catching the error */
+            onError(err);
+          } else {
+            /** Log the error to help debug */
+            console.error(err);
+          }
+          return null;
+        });
     })
     .catch((err) => {
       if (onError) {
