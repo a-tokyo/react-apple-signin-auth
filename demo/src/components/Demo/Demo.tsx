83ee832999db31aa93da8701f855d3f7c4e1071b
Ahmed Tarek <ahmed.tokyo1@gmail.com>
2025-05-28 03:17:06 +0100

    Update Demo.tsx

--- demo/src/components/Demo/Demo.tsx
+++ demo/src/components/Demo/Demo.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
